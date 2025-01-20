8aeb9868edcc4e4822d87c37db0dbfd69367e288
ajoke-naje <ajokenakinseye@gmail.com>
2025-01-19 22:43:52 -0500

    Create functional Tic-Tac Toe in the console

---
+++ script.js
@@ -0,0 +1,145 @@
+const Gameboard = function () {
+    const board = [['', '', ''], ['', '', ''], ['', '', '']];
+    const getBoard = () => board;
+
+    const printBoard = () => {
+        for (let row of board) {
+            console.log(`${row[0]} ${row[1]} ${row[2]}`)
+        }
+    }
+
+    const markBoard = (player, position) => {
+        switch (position) {
+            case 1:
+                if (!board[0][0]) {
+                    board[0][0] = player.symbol;
+                    break;
+                }
+                return false;
+            case 2:
+                if (!board[0][1]) {
+                    board[0][1] = player.symbol;
+                    break;
+                }
+                return false;
+            case 3:
+                if (!board[0][2]) {
+                    board[0][2] = player.symbol;
+                    break;
+                }
+                return false;
+            case 4:
+                if (!board[1][0]) {
+                    board[1][0] = player.symbol;
+                    break;
+                }
+                return false;
+            case 5:
+                if (!board[1][1]) {
+                    board[1][1] = player.symbol;
+                    break;
+                }
+                return false;
+            case 6:
+                if (!board[1][2]) {
+                    board[1][2] = player.symbol;
+                    break;
+                }
+                return false;
+            case 7:
+                if (!board[2][0]) {
+                    board[2][0] = player.symbol;
+                    break;
+                }
+                return false;
+            case 8:
+                if (!board[2][1]) {
+                    board[2][1] = player.symbol;
+                    break;
+                }
+                return false;
+            case 9:
+                if (!board[2][2]) {
+                    board[2][2] = player.symbol;
+                    break;
+                }
+                return false;
+        }
+    }
+
+    const winStates = () => {
+        return [
+            [board[0][0], board[0][1], board[0][2]],
+            [board[1][0], board[1][1], board[1][2]],
+            [board[2][0], board[2][1], board[2][2]],
+            [board[0][0], board[1][0], board[2][0]],
+            [board[0][1], board[1][1], board[2][1]],
+            [board[0][2], board[1][2], board[2][2]],
+            [board[0][0], board[1][1], board[2][2]],
+            [board[0][2], board[1][1], board[2][0]]
+        ]
+    }
+
+    const checkWin = (player) => {
+        for (let state of winStates()) {
+            if (state[0] == state[1] &&
+                 state[0] == state[2] &&
+                 state[1] == state[2] &&
+                 state[0] == player.symbol) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const clearBoard = () => {
+        for (let row = 0; row < 3; row++) {
+            for (let col = 0; col < 3; col++) {
+                board[row][col] = '';
+            }
+        }
+    }
+    
+    return { getBoard, printBoard, markBoard, checkWin, clearBoard };
+}
+
+const Player = function (name, symbol) {
+    let wins = 0;
+    const getWins = () => wins;
+    const winGame = () => wins++;
+    
+    return { name, symbol, getWins, winGame };
+}
+
+const Game = function () {
+    const board = Gameboard();
+    const player1 = Player("Player 1", 'X');
+    const player2 = Player("Player 2", 'O');
+
+    let activePlayer = player1;
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === player1 ? player2 : player1;
+    }
+
+    const playRound = (position) => {
+        if (board.markBoard(activePlayer, position) == false) {
+            console.log("Choose an unoccupied position!");
+        } else {
+            board.printBoard();
+            if (board.checkWin(activePlayer)) {
+                console.log(`${activePlayer.name} wins!`)
+                activePlayer.winGame();
+                console.log(`Score: ${activePlayer.name}-${activePlayer.getWins()}, ${activePlayer == player1 ? player2.name : player1.name}-${activePlayer == player1 ? player2.getWins() : player1.getWins()}`)
+                board.clearBoard();
+            } else {
+                switchPlayerTurn();
+            }
+        }
+    }
+
+    return { playRound }
+
+}
+
+const game = Game();
